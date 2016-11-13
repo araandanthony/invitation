bf38c41d9e1c1854c7fb37687950bf7d6595da62
matapang <anthony.wong06@yahoo.com>
2016-11-13 10:52:33 +0800

    deleted unused image

--- app/invitation/invitation.cmpt.js
+++ app/invitation/invitation.cmpt.js
@@ -18,7 +18,8 @@
                     "img/bg/1.JPG",
                     "img/bg/2.JPG",
                     "img/bg/3.JPG",
-                    "img/bg/4.JPG"
+                    "img/bg/4.JPG",
+                    "img/bg/5.jpg",
                 ], {duration: 5000, fade: 1000, centeredX:true, centeredY:true});
             }
         };
